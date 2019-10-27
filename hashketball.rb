700fcfd440e12110f097b89fd1e2d4e016bd20c9
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-27 22:50:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,7 @@
 def game_hash
   {
-    home: { team_name: 'Brooklyn Nets',
+    home: { 
+      team_name: 'Brooklyn Nets',
       colors: ["Black", "White"],
       players: [
         { player_name: 'Alan Anderson',
@@ -50,7 +51,8 @@ def game_hash
           slam_dunks: 1 }
       ] 
   },
-    away: { team_name: 'Charlotte Hornets',
+    away: { 
+      team_name: 'Charlotte Hornets',
       colors: ["Turquoise", "Purple"],
       players: [
         { player_name: 'Jeff Adrien',
